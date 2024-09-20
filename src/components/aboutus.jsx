f476d5f2b9a927c182eafb10e4ad20f2f5049dee
shubhammjha22 <shubhammjha22@gmail.com>
2024-09-21 00:36:54 +0530

    Made About us responsive SS-1536-1270 ratio fix pending

--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -5,50 +5,88 @@ export default function About() {
   return (
     <>
       <div className="h-[50rem] ">
-        <div className="container  mt-28 mx-auto h-[35rem]  ">
+        <div className="container xl:max-w-[1430px] mt-28 mx-auto h-[35rem]  ">
           <div className="h-64 flex flex-col lg:flex-row items-center justify-between">
-            <div className="flex flex-col items-start">
-              <h1 className="text-5xl  text-[#CA9E57] font-abhaya font-bold  mb-0">
+            <div className="flex flex-col items-start px-4 sm:p-0">
+              <h1 className="text-3xl sm:text-5xl  text-[#CA9E57] font-abhaya font-bold  mb-0">
                 ABOUT YUVAAN
               </h1>
-              <p className="text-2xl text-nowrap text-white font-abhaya mt-0 font-semibold">
-                Yuvaan is the official literature and film festival of dtu which
-                made its debut <br /> in 2017, with captive hearts and minds.
-                <br />
-                Drawing an impressive crowd of over 65,000 attendees, Yuvaan
-                stands as one of <br /> dtu’s most eagerly anticipated cultural
-                events, drawing massive crowds.
-                <br />
-                With it’s engaging events, including slams, debates, quizzes,
-                yuvaan offers <br /> something for every artistic soul.
+              <p className="text-sm sm:text-2xl  sm:text-nowrap text-white font-abhaya mt-0 font-semibold">
+                <span className="sm:hidden">
+                  Yuvaan is the official literature and film festival of DTU,
+                  which made its debut in 2017, with captive hearts and minds.
+                  Drawing an impressive crowd of over 65,000 attendees, Yuvaan
+                  stands as one of DTU's most eagerly anticipated cultural{" "}
+                  events, drawing massive crowds. With its engaging events,
+                  including slams, debates, quizzes, Yuvaan offers something for
+                  every artistic soul.
+                </span>
+
+                {/* Text for screens 'sm' and larger */}
+                <span className="hidden sm:block">
+                  Yuvaan is the official literature and film festival of DTU,
+                  which made its debut <br /> in 2017, with captive hearts and
+                  minds.
+                  <br /> Drawing an impressive crowd of over 65,000 attendees,
+                  Yuvaan stands as one <br /> of DTU's most eagerly anticipated
+                  cultural events, drawing massive crowds.
+                  <br /> With its engaging events, including slams, debates,
+                  quizzes, Yuvaan offers <br /> something for every artistic
+                  soul.
+                </span>
               </p>
             </div>
-            <img src={yuvaan_logo_1} className="h-full " alt="" />
-          </div>
-
-          <div className=" lg:ml-0 h-64 mt-10 flex flex-col lg:flex-row items-center justify-between">
             <img
-              src={LFC}
-              className="-ml-24 -mr-12  xl:ml-0 xl:mr-0 h-[24rem] mx-0"
+              src={yuvaan_logo_1}
+              className="hidden sm:block h-full "
               alt=""
             />
-            <div className="flex flex-col items-end">
-              <h1 className="text-5xl  text-[#CA9E57] font-abhaya font-bold  mb-0">
+          </div>
+
+          <div className=" lg:ml-0 h-64 -mt-10 sm:mt-10 flex flex-col lg:flex-row items-center justify-between">
+            <img
+              src={LFC}
+              className="hidden sm:block -ml-24 -mr-12  xl:ml-0 xl:mr-0 h-[24rem] mx-0"
+              alt=""
+            />
+            <div className="flex flex-col items-end px-3 sm:p-0">
+              <h1 className="relative mr-2 sm:mr-0 text-3xl sm:text-5xl  text-[#CA9E57] font-abhaya font-bold  mb-0">
                 ABOUT LFC
+                <span>
+                  <img
+                    src={LFC}
+                    className="absolute -top-6 -left-16 w-20 h-20 sm:hidden "
+                    alt=""
+                  />
+                </span>
               </h1>
-              <p className="text-2xl text-end text-nowrap text-white font-abhaya mt-0 font-semibold">
-                The Literature and Film Council (LFC) at Delhi Technological
-                University (DTU) <br /> fosters a vibrant cultural environment
-                by promoting literature and cinema among <br /> students. It
-                organizes film screenings, literary events, workshops, and
-                competitions,
-                <br />
-                providing a platform for students to showcase their creativity
-                and engage with the <br />
-                arts. Through these activities, the LFC enriches the university
-                experience and
-                <br /> supports the development of students' literary and
-                cinematic interests.{" "}
+              <p className="text-sm sm:text-2xl sm:text-end sm:text-nowrap text-white font-abhaya mt-0 font-semibold">
+                <span className="sm:hidden">
+                  The Literature and Film Council (LFC) at Delhi Technological
+                  University (DTU) fosters a vibrant cultural environment by
+                  promoting literature and cinema among students. It organizes
+                  film screenings, literary events, workshops, and competitions,
+                  providing a platform for students to showcase their creativity
+                  and engage with the arts. Through these activities, the LFC
+                  enriches the university experience and supports the
+                  development of students' literary and cinematic interests.{" "}
+                </span>
+
+                {/* Text for screens 'sm' and larger */}
+                <span className="hidden sm:block">
+                  The Literature and Film Council (LFC) at Delhi Technological
+                  University (DTU) <br /> fosters a vibrant cultural environment
+                  by promoting literature and cinema among <br /> students. It
+                  organizes film screenings, literary events, workshops, and
+                  competitions,
+                  <br />
+                  providing a platform for students to showcase their creativity
+                  and engage with the <br />
+                  arts. Through these activities, the LFC enriches the
+                  university experience and
+                  <br /> supports the development of students' literary and
+                  cinematic interests.{" "}
+                </span>
               </p>
             </div>
           </div>
--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -3,12 +3,12 @@ import demo from "../assets/demo.png";
 export default function Gallery() {
   return (
     <>
-      <div className="-mt-40  relative h-[90rem] xl:h-[100rem] ">
-        <div className="container h-full mx-auto bg-red-00 ">
+      <div className=" -mt-[20rem] h-[75rem]  sm:-mt-40  relative sm:h-[108rem] ">
+        <div className="container xl:max-w-[1430px] h-full mx-auto bg-red-00 ">
           {/* background layer */}
-          <div className="h-[60%] xl:h-[65%] container absolute mx-auto bg-[#000000] opacity-[21%]"></div>
+          <div className="h-[68%] xl:max-w-[1430px] sm:h-[66%] container absolute xl:mx-auto bg-[#000000] opacity-[21%]"></div>
           {/* Gallery section */}
-          <div className="grid grid-cols-5 p-5 gap-5">
+          <div className="grid grid-cols-3 sm:grid-cols-5 p-5 gap-6 sm:gap-5">
             <img src={demo} alt="" />
             <img src={demo} alt="" />
             <img src={demo} alt="" />
@@ -19,16 +19,25 @@ export default function Gallery() {
             <img src={demo} alt="" />
             <img src={demo} alt="" />
             <img src={demo} alt="" />
+            <span className="sm:hidden" alt="" />
+            <img src={demo} className="sm:hidden" alt="" />
           </div>
-          <div className="container px-4 h-[11.5rem]  flex justify-between items-center">
+          <div className="container  mx-auto px-4 h-[4.7rem]  w-full xl:h-[12.75rem] sm:h-[11.5rem]  flex justify-between items-center">
             <img src={demo} className="h-full" alt="" />
-            <h1 className="text-6xl text-center text-[#CA9E57] z-20 font-abhaya font-bold  mb-0">
-              GALLERY
+            <h1 className="text-3xl  -mt-24 sm:mt-0 gap-0 tightly w-full sm:text-6xl text-center text-[#CA9E57] z-20 font-abhaya font-bold mb-0 sm:block flex flex-col items-center">
+              <span>G</span>
+              <span className="-mt-2">A</span>
+              <span className="-mt-2">L</span>
+              <span className="-mt-2">L</span>
+              <span className="-mt-2">E</span>
+              <span className="-mt-2">R</span>
+              <span className="-mt-2">Y</span>
             </h1>
+
             <img src={demo} className="h-full" alt="" />
           </div>
 
-          <div className="grid grid-cols-5 p-5  gap-5">
+          <div className="grid grid-cols-3 sm:grid-cols-5 p-5 gap-6 sm:gap-5">
             <img src={demo} alt="" />
             <img src={demo} alt="" />
             <img src={demo} alt="" />
@@ -38,14 +47,18 @@ export default function Gallery() {
             <img src={demo} alt="" />
             <img src={demo} alt="" />
             <img src={demo} alt="" />
-            <img src={demo} alt="" />
+            <img src={demo} className="hidden sm:block" alt="" />
           </div>
 
-          <div className="mt-10 flex flex-col justify-center items-center z-40 absolute container mx-auto h-[28rem]">
-            <h1 className="text-6xl  text-[#CA9E57] font-abhaya font-bold  mb-0">
+          <div className="-mt-[5rem] sm:mt-10 flex flex-col justify-center items-center z-40 absolute container mx-auto h-[28rem]">
+            <h1 className="text-2xl sm:text-6xl  text-[#CA9E57] font-abhaya font-bold  mb-0">
               AFTER MOVIE
             </h1>
-            <img src={demo} className="w-[48rem] max-h-[20rem]" alt="" />
+            <img
+              src={demo}
+              className="h-[12rem] sm:w-[48rem] sm:min-h-[20rem]"
+              alt=""
+            />
           </div>
         </div>
       </div>
