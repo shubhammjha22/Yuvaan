cc52640c40628944961f74a1c47dc8e1dd470765
shubhammjha22 <shubhammjha22@gmail.com>
2024-10-01 08:55:24 +0530

    added loader, will use in feature

---
+++ src/components/loader.jsx
@@ -0,0 +1,25 @@
+import website_texture from "/assets/website_texture.png";
+
+export default function Loader() {
+  return (
+    <>
+      <div className="relative h-[100vh]   overflow-x-hidden bg-[#2A0B44]">
+        <div
+          className="absolute inset-0"
+          style={{
+            backgroundImage: `url(${website_texture})`, // Texture image (white dots)
+            backgroundSize: "cover", // Ensures the texture covers the entire area
+            backgroundRepeat: "repeat", // Repeats the texture
+            // Adjust the opacity so the gradient shows through
+          }}
+        />
+        <div className="h-[100vh] flex justify-center items-center">
+          <h1 className="text-5xl sm:text-7xl  text-[#CA9E57] font-abhaya font-bold  ">
+            ...
+          </h1>
+        </div>
+        <div className="bg-footer-gradient w-full h-1/3 absolute bottom-0 "></div>
+      </div>
+    </>
+  );
+}
