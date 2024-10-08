34c40d8f58779fd34c07a307ef9897806bb420a0
shubhammjha22 <shubhammjha22@gmail.com>
2024-10-08 12:05:08 +0530

    made contact us page responsive

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -55,17 +55,18 @@ const data = [
     imgUrl:
       "https://res.cloudinary.com/derpoctie/image/upload/v1727728610/Rectangle_39_no1nov.png",
   },
-  {
-    name: "HARSHIT GUPTA",
-    insta: "https://www.instagram.com/_harshit.007/",
-    imgUrl:
-      "https://res.cloudinary.com/derpoctie/image/upload/v1727728614/Rectangle_35_h1iryz.png",
-  },
+
   {
     name: "PRIYANSHU",
     insta: "https://www.instagram.com/",
     imgUrl:
       "https://res.cloudinary.com/derpoctie/image/upload/v1727728615/Rectangle_36_zfjfrv.png",
+  },
+  {
+    name: "HARSHIT GUPTA",
+    insta: "https://www.instagram.com/_harshit.007/",
+    imgUrl:
+      "https://res.cloudinary.com/derpoctie/image/upload/v1727728614/Rectangle_35_h1iryz.png",
   },
   {
     name: "TANISH SHARMA",
@@ -85,16 +86,16 @@ export default function Contacts() {
   return (
     <>
       <div className="mt-24 sm:mt-16 flex flex-col justify-center  items-center">
-        <h1 className="text-[30px] sm:text-[100px] opacity-70  text-white font-abhaya font-extrabold">
+        <h1 className="text-3xl sm:text-7xl opacity-70  text-white font-abhaya font-extrabold">
           COUNCIL MEMBERS 24-25
         </h1>
 
         {/* PRESIDENT */}
-        <div className="flex flex-col min-w-[225px] w-[60%] mt-4 sm:mt-0 sm:w-[45%]">
+        <div className="flex flex-col min-w-[225px] w-[90%] mt-4 sm:mt-0 sm:w-[45%]">
           <h1 className="text-3xl sm:text-7xl text-center  text-[#CA9E57] font-abhaya font-bold">
             PRESIDENT
           </h1>
-          <div className="flex sm:flex-row flex-col mt-2 sm:justify-between gap-10 sm:items-center">
+          <div className="flex sm:flex-row mt-2 justify-between gap-10 sm:items-center">
             <ImgCard
               name="SAHIL YADAV"
               imgUrl="https://res.cloudinary.com/derpoctie/image/upload/v1727717000/Sahil_rbghjd.png"
@@ -106,11 +107,11 @@ export default function Contacts() {
 
         {/* TREASURER */}
 
-        <div className="mt-16 flex flex-col min-w-[225px] w-[60%] sm:w-[45%]">
+        <div className="mt-16 flex flex-col min-w-[225px] w-[90%] sm:w-[45%]">
           <h1 className="text-3xl sm:text-7xl text-center  text-[#CA9E57] font-abhaya font-bold">
             TREASURER
           </h1>
-          <div className="flex sm:flex-row flex-col mt-2 justify-between gap-10 items-center">
+          <div className="flex sm:flex-row  mt-2 justify-between gap-10 items-center">
             <ImgCard {...data[1]} />
             <ImgCard {...data[2]} />
           </div>
@@ -118,21 +119,26 @@ export default function Contacts() {
 
         {/* VICE PRESIDENT */}
 
-        <div className="mt-16 flex flex-col min-w-[225px] w-[60%] sm:w-[65%]">
+        <div className="mt-16 flex flex-col min-w-[225px] w-[90%] sm:w-[65%]">
           <h1 className="text-3xl sm:text-7xl text-center  text-[#CA9E57] font-abhaya font-bold">
             VICE PRESIDENT
           </h1>
-          <div className="flex sm:flex-row flex-col mt-2 gap-10 justify-between items-center">
+          <div className="flex sm:flex-row  mt-2 gap-10 justify-between items-center">
             <ImgCard {...data[3]} />
             <ImgCard {...data[4]} />
+            <span className="hidden sm:block">
+              <ImgCard {...data[5]} />
+            </span>
+          </div>
+          <div className=" sm:hidden w-[45%] mt-4 mx-auto">
             <ImgCard {...data[5]} />
           </div>
         </div>
 
         {/* Joint Treasurer */}
 
-        <div className="mt-16 flex flex-col min-w-[225px] w-[60%] sm:w-[45%]">
-          <div className="flex sm:flex-row flex-col mt-2 justify-between gap-16 items-center">
+        <div className="mt-16 flex flex-col min-w-[225px] w-[90%] sm:w-[45%]">
+          <div className="flex sm:flex-row  mt-2 justify-between gap-16 items-center">
             <ImgCard {...data[6]} heading="JOINT TREASURE" />
 
             <ImgCard {...data[7]} heading="JOINT TREASURE" />
@@ -141,18 +147,26 @@ export default function Contacts() {
 
         {/* Directors */}
 
-        <div className="mt-24 flex flex-col min-w-[225px] w-[60%] sm:w-[65%]">
-          <div className="flex sm:flex-row flex-col mt-2 justify-between gap-24 items-center">
+        <div className="mt-24 flex flex-col min-w-[225px] w-[90%] sm:w-[65%]">
+          <div className="flex sm:flex-row  mt-2 justify-between gap-14 sm:gap-24 items-center">
             <ImgCard {...data[8]} heading="DIRECTOR OF DESIGN" />
 
-            <ImgCard {...data[9]} heading="DIRECTOR OF EVENT AND LOGISTICS" />
-            <ImgCard {...data[10]} heading="DIRECTOR OF CREATIVE" />
+            <ImgCard {...data[9]} heading="DIRECTOR OF CREATIVE" />
+            <span className="hidden sm:block">
+              <ImgCard
+                {...data[10]}
+                heading="DIRECTOR OF EVENT AND LOGISTICS"
+              />
+            </span>
+          </div>
+          <div className=" sm:hidden w-[45%] mt-20 mx-auto">
+            <ImgCard {...data[10]} heading="DIRECTOR OF EVENT AND LOGISTICS" />
           </div>
         </div>
 
         {/* Logistics and Outreach */}
-        <div className="mt-24 flex flex-col min-w-[225px] w-[60%] sm:w-[45%]">
-          <div className="flex sm:flex-row flex-col mt-2 justify-between gap-24 items-center">
+        <div className="mt-24 flex flex-col min-w-[225px] w-[90%] sm:w-[45%]">
+          <div className="flex sm:flex-row mt-2 justify-between gap-16 items-center">
             <ImgCard {...data[11]} heading="DIRECTOR OF LOGISTICS" />
 
             <ImgCard {...data[12]} heading="DIRECTOR OF OUTREACH" />
--- src/components/imgcard.jsx
+++ src/components/imgcard.jsx
@@ -2,14 +2,14 @@ export default function ImgCard({ imgUrl, name, insta, heading }) {
   return (
     <div>
       <div className="relative text-center">
-        <img src={imgUrl} alt={name} className="h-64 relative" />
-        <div className="w-full bg-[#D9D9D9] rounded-bl-3xl rounded-br-3xl absolute bottom-0 flex flex-col justify-center">
-          <h2 className="relative font-abhaya font-bold text-2xl text-center mb-0">
+        <img src={imgUrl} alt={name} className="sm:h-64 relative" />
+        <div className="w-full bg-[#D9D9D9] sm:rounded-bl-3xl  sm:rounded-br-3xl rounded-bl-2xl rounded-br-2xl  absolute bottom-0 flex flex-col justify-center">
+          <h2 className="relative font-abhaya font-bold text-sm sm:text-2xl text-center mb-0">
             {name}
           </h2>
           <a
             href={insta}
-            className="h-6 w-6 mx-auto mix-blend-difference mb-1 -mt-1"
+            className="h-4 w-4 sm:h-6 sm:w-6 mx-auto mix-blend-difference mb-1 -mt-1"
             target="_blank"
           >
             <img
@@ -21,7 +21,7 @@ export default function ImgCard({ imgUrl, name, insta, heading }) {
         </div>
         {heading && (
           <div className="absolute w-full mt-2 flex justify-center">
-            <h1 className="text-center absolute w-[135%] text-3xl  text-[#CA9E57] font-abhaya font-bold">
+            <h1 className="text-center absolute w-[135%] text-lg sm:text-3xl  text-[#CA9E57] font-abhaya font-bold">
               {heading}
             </h1>
           </div>
