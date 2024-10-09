524f9d7fdcee94361ee67c231b1626d37c50a61b
shubhammjha22 <shubhammjha22@gmail.com>
2024-10-10 00:17:49 +0530

    made layout of accomodation

--- src/pages/Accommodations.jsx
+++ src/pages/Accommodations.jsx
@@ -1,9 +1,32 @@
-import ComingSoon from "./ComingSoon";
+import SocialMedia from "../components/socialMedia";
+
+const bgUrl =
+  "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728498473/Accomm-desktop_hdducr.png";
 
 export default function Accommodations() {
   return (
     <>
-      <ComingSoon />
+      <div className="h-screen w-screen bg-[#2A0B44]">
+        <section className="flex flex-col justify-evenly sm:block relative h-full overflow-hidden">
+          <img
+            src={bgUrl}
+            className="absolute inset-0 z-0 h-[100vh] w-[100vw] -tp-40"
+            alt=""
+          />
+          <div className="mt-24 relative sm:mt-24 flex flex-col justify-center  items-center">
+            <h1 className="text-8xl   font-abhaya font-extrabold text-[#CA9E57]">
+              ACCOMMODATIONAS
+            </h1>
+            <h3 className="text-5xl -mt-6 font-abhaya font-semibold text-white">
+              AT 7TH EDITION
+            </h3>
+          </div>
+
+          <div className="absolute bottom-10 left-16">
+            <SocialMedia />
+          </div>
+        </section>
+      </div>
     </>
   );
 }
