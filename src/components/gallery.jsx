71554cb31718c6e621a679df88aca4c7c445822c
shubhammjha22 <shubhammjha22@gmail.com>
2024-10-10 11:45:32 +0530

    added img of gallery

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -3,6 +3,97 @@ import AfterMovie from "./afterMovie";
 const demo =
   "https://res.cloudinary.com/derpoctie/image/upload/v1727753742/demo_fzapwb.png";
 
+const imgUrl = [
+  {
+    id: 1,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533883/Rectangle_43_uck1yw.png",
+  },
+  {
+    id: 2,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533889/Rectangle_44_iukhix.png",
+  },
+  {
+    id: 3,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533888/Rectangle_45_etuuon.png",
+  },
+  {
+    id: 4,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533892/Rectangle_46_zgjbqv.png",
+  },
+  {
+    id: 5,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533886/Rectangle_47_fzrbf4.png",
+  },
+  {
+    id: 6,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533891/Rectangle_48_wjnnqt.png",
+  },
+  {
+    id: 7,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533888/Rectangle_49_alfed9.png",
+  },
+  {
+    id: 8,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533881/Rectangle_50_dlijcv.png",
+  },
+  {
+    id: 9,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533892/Rectangle_51_mgk1zv.png",
+  },
+  {
+    id: 10,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533884/Rectangle_52_ljijmj.png",
+  },
+  {
+    id: 11,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533888/Rectangle_63_e3ocoo.png",
+  },
+  {
+    id: 12,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533887/Rectangle_64_sbqeim.png",
+  },
+  {
+    id: 13,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533882/Rectangle_53_sey6vu.png",
+  },
+  {
+    id: 14,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533885/Rectangle_54_v6iqdm.png",
+  },
+  {
+    id: 15,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533883/Rectangle_55_eyigzs.png",
+  },
+  {
+    id: 16,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533881/Rectangle_59_ja3h6y.png",
+  },
+  {
+    id: 17,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533879/Rectangle_57_dzgc7l.png",
+  },
+  {
+    id: 18,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533881/Rectangle_58_uqu27b.png",
+  },
+  {
+    id: 19,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533880/Rectangle_56_ffk8be.png",
+  },
+  {
+    id: 20,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533882/Rectangle_60_vbadk0.png",
+  },
+  {
+    id: 21,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533882/Rectangle_61_idv3we.png",
+  },
+  {
+    id: 22,
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728533882/Rectangle_62_p1yok9.png",
+  },
+];
+
 export default function Gallery() {
   return (
     <>
@@ -12,36 +103,47 @@ export default function Gallery() {
 
           <div className="bg-[#000000] z-20 opacity-[21%]  sm:ml-0 w-[95%] sm:w-full xl:w-[95%] p-10 absolute mx-auto h-full xl:h-[94%]    "></div>
           {/* Gallery section */}
-          <div className="z-10 grid grid-cols-3 sm:grid-cols-5 p-4 gap-5 sm:gap-2">
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
+          <div className="z-40 grid grid-cols-3 sm:grid-cols-5 p-4 gap-5 sm:gap-2">
+            {imgUrl.slice(0, 11).map((item) => (
+              <img
+                key={item.id}
+                src={item.url}
+                className="aspect-square relative "
+                alt={item.id}
+              />
+            ))}
+
             {/* Gallery effects s */}
             <span className="hidden sm:inline" alt="" />
             <span className="hidden sm:inline" alt="" />
             <span className="hidden sm:inline" alt="" />
 
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
+            {imgUrl.slice(11, 13).map((item) => (
+              <img
+                key={item.id}
+                src={item.url}
+                className="aspect-square"
+                alt={item.id}
+              />
+            ))}
             <span className="inline sm:hidden" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
+            {imgUrl.slice(13, 15).map((item) => (
+              <img
+                key={item.id}
+                src={item.url}
+                className="aspect-square"
+                alt={item.id}
+              />
+            ))}
             <span className="inline sm:hidden" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
-            <img src={demo} alt="" />
+            {imgUrl.slice(15).map((item) => (
+              <img
+                key={item.id}
+                src={item.url}
+                className="aspect-square"
+                alt={item.id}
+              />
+            ))}
           </div>
 
           {/* Diff gallery text for different dimension - Look for improvement */}
