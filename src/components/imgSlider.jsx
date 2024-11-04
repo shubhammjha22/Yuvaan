8ed79519602ca8b47aa4090c23945d2d6ba79ccc
shubhammjha22 <shubhammjha22@gmail.com>
2024-11-04 23:33:07 +0530

    added new img and changed date

--- src/components/imgSlider.jsx
+++ src/components/imgSlider.jsx
@@ -3,21 +3,22 @@ import Slider from "react-slick";
 
 // Images from Cloudinary
 const images = [
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390567/Rectangle_70_aumbcx.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390567/Rectangle_71_opirjg.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390560/Rectangle_72_p6ceez.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390560/Rectangle_73_mpu3lv.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390561/Rectangle_74_dyefga.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390561/Rectangle_75_ixpslo.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390562/Rectangle_76_ogmgcl.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390562/Rectangle_77_xmg2di.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390561/Rectangle_78_izmm9o.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390562/Rectangle_79_nonb6v.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390562/Rectangle_80_loihxf.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390563/Rectangle_81_gmqblh.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390564/Rectangle_82_rp1roz.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390565/Rectangle_83_bg8dzu.png",
-  "https://res.cloudinary.com/derpoctie/image/upload/v1728390566/Rectangle_84_eufu4c.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740731/Bayaan_r6ukit.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740730/REALMS_OF_IMAGINATION_n1faya.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740729/BASANTI_POUNCE_MAT_KARNA_zsxmqw.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740728/DELTECH_DEBATATHON_tyyecx.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740732/SOLO_FOLK_COMPETITION_ajrrm4.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740730/NAYAAB_tdt47q.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740728/DUET_AND_GROUP_CLASSICAL_COMPETITION_qb1vxs.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740728/CLASH_OF_COMICS_kk7dpw.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740728/CONSULTICA_CHALLENGE_ckyndg.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740730/LENS_LEGENDS_vk10bs.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740731/The_Muse_Matter_Hindi_jqbjyh.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740732/The_Muse_Matter_gxjnia.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740728/JUST_A_MINUTE_eifecf.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740729/BOOKMARK_MAKING_vhyxvl.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740730/PHOTO_EXHIBITION_k9l178.png",
+  "https://res.cloudinary.com/derpoctie/image/upload/v1730740729/16_hnozqb.png",
 ];
 
 export default function ImageSlider({ setActiveImage, activeImage }) {
@@ -96,7 +97,7 @@ export default function ImageSlider({ setActiveImage, activeImage }) {
               <img
                 src={img}
                 alt={`movie ${index}`}
-                className={`relative w-full h-full sm:w-[13rem] sm:h-[14rem] hover:cursor-pointer transition-all ease-in  hover:opacity-100 hover:scale-105 active:scale-95 active:duration-100  duration-300 ${
+                className={`relative w-full h-full sm:w-[13rem] sm:h-[14rem] rounded-xl hover:cursor-pointer transition-all ease-in  hover:opacity-100 hover:scale-105 active:scale-95 active:duration-100  duration-300 ${
                   index === activeImage ? "opacity-[100%]" : "opacity-[60%]"
                 }  `}
                 style={{ willChange: "opacity, transform" }}
--- src/pages/Competition.jsx
+++ src/pages/Competition.jsx
@@ -8,7 +8,7 @@ const backgroundImages = [
     title: "Rap Battle",
     description:
       "A rap battle competition. Showcase your  lyrical skills and battle other rappers on stage.",
-    date: "19th October",
+    date: "12th November",
     venue: "B.R. Auditorum",
     cash: "10K",
     link: "https://unstop.com/p/bayaan-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1172677",
@@ -19,7 +19,7 @@ const backgroundImages = [
     title: "Short Film Challenge",
     description:
       "Showcase your filmmaking talent in our on campus short film challenge! Create a captivating film based on one of five themes and display your creativity and technical skills.",
-    date: "18th to 20 October",
+    date: "11th to 13th November",
     venue: "Mini OAT",
     cash: "10K",
     link: "https://unstop.com/p/short-film-challenge-realms-of-imagination-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1172238",
@@ -31,7 +31,7 @@ const backgroundImages = [
     title: "Mela Quiz",
     description:
       "Show off what you know about Bollywood, Media, Entertainment, Literature, and Arts",
-    date: "18th October",
+    date: "11th November",
     venue: "SPS 13",
     cash: "10K",
     link: "https://unstop.com/p/basanti-pounce-mat-karna-literature-and-film-council-dtu-yuvaan-1172876",
@@ -42,7 +42,7 @@ const backgroundImages = [
     title: "Debate Competition",
     description:
       "Showcase your debating skills and tackle real world challenges in the DelTech Debatathon",
-    date: "18th-19th October",
+    date: "11th-12th November",
     venue: "Academic Block",
     cash: "10K",
     link: "https://unstop.com/p/deltech-debatathon-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1174012",
@@ -52,7 +52,7 @@ const backgroundImages = [
     heading: "SOLO FOLK COMPETITION",
     title: "Dance Competition",
     description: "Showcase your mastery of solo Indian folk dance at our event",
-    date: "19th October",
+    date: "12th November",
     venue: "B.R. Auditorum",
     cash: "10K",
     link: "https://unstop.com/p/solo-folk-competition-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1172664",
@@ -63,7 +63,7 @@ const backgroundImages = [
     title: "Fashion Show",
     description:
       "Showcase your designs inspired by India's fashion history. Blend tradition with innovation and tell a story through each look. ",
-    date: "20th October",
+    date: "13th November",
     venue: "B.R. Auditorum",
     cash: "15K",
     link: "https://unstop.com/p/nayaab-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1172262",
@@ -75,7 +75,7 @@ const backgroundImages = [
     title: "Debate Competition",
     description:
       "Flaunt your mastery of Indian Classical Dance at our event. Perform any of the eight recognized forms or their fusion, set to traditional musice only",
-    date: "19th October",
+    date: "12th November",
     venue: "B.R. Auditorum",
     cash: "15K",
     link: "https://unstop.com/p/duet-and-group-classical-competition-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1172645",
@@ -86,7 +86,7 @@ const backgroundImages = [
     title: "Standup",
     description:
       "Showcase your unique talent and style! Share stories, humor, or spoken word performances. Break the ordinary. embrace diversity, and let your creativity shine",
-    date: "18th-20th October",
+    date: "11th-13th November",
     venue: "B.R. Auditorum",
     cash: "10K",
     link: "https://unstop.com/p/clash-of-comics-standup-comedy-competition-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1172456",
@@ -97,7 +97,7 @@ const backgroundImages = [
     title: "Case Solving Competition",
     description:
       "Showcase your problem solving skills, analyze business cases and present their solutions.",
-    date: "19th October",
+    date: "12th November",
     venue: "SPS 13",
     cash: "10K",
     link: "https://unstop.com/p/consultica-challenge-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1172484",
@@ -108,7 +108,7 @@ const backgroundImages = [
     title: "Photostory Competition",
     description:
       'Showcase your photography skills by submitting 5-7 photos that capture the essence of the festival, focusing on the themes "Festival Moments" or "Colors of Celebration"',
-    date: "18th-20th October",
+    date: "11th-13th November",
     venue: "Mini OAT",
     cash: "10K",
     link: "https://unstop.com/p/lens-legends-photo-story-competition-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1171852",
@@ -119,7 +119,7 @@ const backgroundImages = [
     title: "Poetry (Hindi) Competition",
     description:
       'Showcase your poetic talent in our "The Muse Matter" competition. Write an original Hindi poem inspired by a randomly selected object and perform it on stage.',
-    date: "20th October",
+    date: "13th November",
     venue: "Convocation Hall",
     cash: "10K",
     link: "https://unstop.com/p/kavatashhanae-literature-and-film-council-dtu-yuvaan-1172895",
@@ -131,7 +131,7 @@ const backgroundImages = [
     title: "Poetry (English) Competition",
     description:
       'Showcase your poetic talent in our "The Muse Matter" competition. Write an original poem inspired by a randomly selected object and perform it on stage.',
-    date: "20th October",
+    date: "13th November",
     venue: "Convocation Hall",
     cash: "10K",
     link: "https://unstop.com/p/the-muse-matter-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1172887",
@@ -142,7 +142,7 @@ const backgroundImages = [
     title: "Master the minute",
     description:
       "Participate in JAM, a solo speaking competition at Sahitya. Speak on a given topic for one minute without hesitation, repetition, or deviation.",
-    date: "18th October",
+    date: "11th November",
     venue: "Convocation Hall",
     cash: "4K",
     link: "https://unstop.com/p/just-a-minute-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1174443",
@@ -153,7 +153,7 @@ const backgroundImages = [
     title: "Bookmark Making",
     description:
       "Showcase your creativity in our art competition, design a bookmark (traditionally or digitally) based on a theme announced on the day.",
-    date: "19th October",
+    date: "12th November",
     venue: "Convocation Hall",
     cash: "10K",
     link: "https://unstop.com/p/bookmark-making-yuvaan-7th-edition-literature-and-film-council-dtu-yuvaan-1174641",
@@ -164,10 +164,21 @@ const backgroundImages = [
     title: "Exhibition",
     description:
       "Exhibit your captivating shots to celebrate the artistry of cinematography and photography.",
-    date: "18th to 20th October",
+    date: "11th to 13th November",
     venue: "Mini OAT",
     cash: null,
     link: "https://forms.gle/8CZtSpHkhqfhhJAz8",
+    url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728492170/PHOTO_EXHIBITION_pj2jmj.png",
+  },
+  {
+    heading: "PITCH-A-THON",
+    title: "Exhibition",
+    description:
+      "Unleash your ideas💡 at Pitch-a-thon: Where innovation meets opportunity! Bring your vision, showcase your innovation.Inspire, compete, and impress the best.Turn your concepts into reality!",
+    date: "11th November",
+    venue: "SPS-12",
+    cash: "10K",
+    link: "",
     url: "https://res.cloudinary.com/derpoctie/image/upload/q_auto,f_auto/v1728492170/PHOTO_EXHIBITION_pj2jmj.png",
   },
 ];
